db89c4bc0cb6f2c3e57f312758a3757a2d836888
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-15 14:31:05 +0100

    polished Services

--- src/sections/services/data.js
+++ src/sections/services/data.js
@@ -2,20 +2,21 @@ import {SiAdobexd} from 'react-icons/si'
 import {RiReactjsLine} from 'react-icons/ri'
 import {FaServer} from 'react-icons/fa'
 import {AiFillAppstore} from 'react-icons/ai'
+import {FaMicroscope} from 'react-icons/fa'
 
 
 const data = [
     {
-        id: 1, icon: <SiAdobexd/>, title: 'UI/UX Design', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
+        id: 1, icon: <SiAdobexd/>, title: 'UI/UX Design', desc: "My designs are modern and functional. I use industry-standard rules to make sure your users have a great experience while getting their work done with the applicaiton."
     },
     {
-        id: 2, icon: <RiReactjsLine/>, title: 'Frontend Development', desc: "Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop."
+        id: 2, icon: <AiFillAppstore/>, title: 'Product Management', desc: "My focus is to help you know discover your product strategy, know the users we will be buiding for, map every interaction - even if offline and then build a proper Roadmap."
     },
     {
-        id: 3, icon: <FaServer/>, title: 'Backend Development', desc: "The security of your business/product is taken seriously right from the start of the project. I will make sure your website/app is secure from attacks."
+        id: 3, icon: <FaMicroscope/>, title: 'UX Research', desc: "Collecting relevant qualitative and quantitative data to make the best decisions is one of the toughest task in Product development, I help you collect, clean and distill insights from your users."
     },
     {
-        id: 4, icon: <AiFillAppstore/>, title: 'App Development', desc: 'Unlike other developers in the market, I will building an app that runs on both IOS and Android devices without any extra cost to you.'
+        id: 4, icon: <RiReactjsLine/>, title: 'Frontend Development', desc: 'Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop..'
     }
 ]
 
