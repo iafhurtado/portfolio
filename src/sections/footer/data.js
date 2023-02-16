ff023b4d8e37c68a114520cf02945bded2b78034
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-16 15:28:50 +0100

    adjusted linkedin icon

--- src/sections/footer/data.js
+++ src/sections/footer/data.js
@@ -1,4 +1,4 @@
-import {AiOutlineLinkedin} from 'react-icons/ai'
+import {TfiLinkedin} from 'react-icons/tfi'
 import {AiOutlineTwitter} from 'react-icons/ai'
 import {GrMedium} from 'react-icons/gr'
 import {AiFillGithub} from 'react-icons/ai'
@@ -13,7 +13,7 @@ export const links = [
 
 
 export const socials = [
-    {id: 1, link: 'https://www.linkedin.com/in/ivan-flores-hurtado-09243b44/', icon: <AiOutlineLinkedin/>},
+    {id: 1, link: 'https://www.linkedin.com/in/ivan-flores-hurtado-09243b44/', icon: <TfiLinkedin/>},
     {id: 2, link: 'https://twitter.com/iafhurtado', icon: <AiOutlineTwitter/>},
     {id: 3, link: 'https://medium.com/@ivanalfredo', icon: <GrMedium/>},
     {id: 4, link: 'https://github.com/iafhurtado', icon: <AiFillGithub/>}
--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,10 +1,10 @@
-import {AiOutlineLinkedin} from 'react-icons/ai'
+import {TfiLinkedin} from 'react-icons/tfi'
 import {AiOutlineTwitter} from 'react-icons/ai'
 import {GrMedium} from 'react-icons/gr'
 import {AiFillGithub} from 'react-icons/ai'
 
 const data = [
-    {id: 1, link: 'https://www.linkedin.com/in/ivan-flores-hurtado-09243b44/', icon: <AiOutlineLinkedin/>},
+    {id: 1, link: 'https://www.linkedin.com/in/ivan-flores-hurtado-09243b44/', icon: <TfiLinkedin/>},
     {id: 2, link: 'https://twitter.com/iafhurtado', icon: <AiOutlineTwitter/>},
     {id: 3, link: 'https://medium.com/@ivanalfredo', icon: <GrMedium/>},
     {id: 4, link: 'https://github.com/iafhurtado', icon: <AiFillGithub/>}
