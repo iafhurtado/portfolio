f3f0937be96af8c68bc5a5330c44ca04de9031c1
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-16 15:26:54 +0100

    replaced dribbble with medium

--- src/sections/footer/data.js
+++ src/sections/footer/data.js
@@ -1,6 +1,6 @@
 import {AiOutlineLinkedin} from 'react-icons/ai'
 import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiOutlineDribbble} from 'react-icons/ai'
+import {GrMedium} from 'react-icons/gr'
 import {AiFillGithub} from 'react-icons/ai'
 
 export const links = [
@@ -15,6 +15,6 @@ export const links = [
 export const socials = [
     {id: 1, link: 'https://www.linkedin.com/in/ivan-flores-hurtado-09243b44/', icon: <AiOutlineLinkedin/>},
     {id: 2, link: 'https://twitter.com/iafhurtado', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com/iafhurtado', icon: <AiOutlineDribbble/>},
+    {id: 3, link: 'https://medium.com/@ivanalfredo', icon: <GrMedium/>},
     {id: 4, link: 'https://github.com/iafhurtado', icon: <AiFillGithub/>}
 ]
