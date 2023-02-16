df342d1e4a9556cc45ae6d323174d2198edf3cca
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-16 15:07:26 +0100

    almost done with projects

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -6,7 +6,7 @@ import Image5 from '../../assets/project5.jpg'
 import Image6 from '../../assets/project6.jpg'
 import Image7 from '../../assets/project7.jpg'
 import Image8 from '../../assets/project8.jpg'
-import Image9 from '../../assets/project9.jpg'
+import Image9 from '../../assets/project9.png'
 
 const data = [
     {
@@ -15,51 +15,51 @@ const data = [
         image: Image1,
         title: "Web & Mobile Application (UIUX)",
         desc: "The solution was to create a web app using React and a mobile app using Iconic which are both light-weight open-source software frameworks that allowed us to quickly deploy components that are easy to use and can be stacked together easily as the application grows in size.",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        demo: 'https://www.figma.com/proto/BhN4t76z2EBzIj3wzV5Uhd/PMS-Tickettool?page-id=0%3A1&node-id=31%3A104&viewport=405%2C131%2C0.03&scaling=contain',
+        github: 'https://www.figma.com/file/BhN4t76z2EBzIj3wzV5Uhd/PMS-Tickettool?t=dOCla21egILpWCb0-1'
     },
     {
         id: 2,
-        category: 'frontend',
+        category: 'product management',
         image: Image2,
-        title: "DeFi Portfolio Tracker (Frontend)",
+        title: "DeFi Portfolio Tracker (Product Management)",
         desc: "The task was to build a DeFi Portfolio Tracker a.k.a. an application to track an EVM wallet similar to what Zapper and Zerion do today but with more granular and specific data insights. The project was to perform Discovery for this application before building it.",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        demo: 'https://www.figma.com/proto/mfIrvQBy4ni3RadCfR9MCU/DeFi-Portfolio-Tracker?page-id=0%3A1&node-id=195%3A84&viewport=751%2C485%2C0.11&scaling=min-zoom',
+        github: 'https://www.figma.com/file/mfIrvQBy4ni3RadCfR9MCU/DeFi-Portfolio-Tracker?node-id=708%3A9717&t=SSxJsPrfAkziF54T-1'
     },
     {
         id: 3,
-        category: 'frontend',
+        category: 'product management',
         image: Image3,
-        title: "Fulfillment Integration App (Frontend)",
+        title: "Fulfillment Integration App (Product Management)",
         desc: "The goal of this project was to draft a 5-10 page PRD for an e-commerce fulfillment integration in the Shopify appstore. In the first phase of this project, I was asked with benchmarking other apps in this space and then document how this application's features and functionality would look like.",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        demo: 'https://www.figma.com/proto/5Ht4EtQC2wHsN2gBPBXBHy/Fulfillment-Integration-App?page-id=0%3A1&node-id=901%3A2&viewport=817%2C429%2C0.3&scaling=min-zoom',
+        github: 'https://www.figma.com/file/5Ht4EtQC2wHsN2gBPBXBHy/Fulfillment-Integration-App?node-id=901%3A2&t=mDzdGJdCLs9IhmSz-1'
     },
     {
         id: 4,
         category: 'uiux',
         image: Image4,
         title: "Whistleblower App (UIUX)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        desc: "The goal of this project was to design a whistleblower app that would allow users to report any wrongdoing in their organization. The app would allow users to report anonymously and would allow the organization to track the progress of the report.",
+        demo: 'https://www.figma.com/proto/5jqZRHGnLA4mvxWIi49y6f/Whistleblower-App?page-id=0%3A1&node-id=148%3A167&viewport=1176%2C424%2C0.1&scaling=min-zoom',
+        github: 'https://www.figma.com/file/5jqZRHGnLA4mvxWIi49y6f/Whistleblower-App?node-id=148%3A167&t=ZdzJtfFZFu0ysLhg-1'
     },
     {
         id: 5,
-        category: 'uiux',
+        category: 'product management',
         image: Image5,
-        title: "Privacy and Compliance Platform (UIUX)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Privacy and Compliance Platform (Product Management)",
+        desc: "The goal of this project was to design a privacy and compliance platform that would allow users to manage their privacy and compliance needs. The platform would allow users to manage their privacy and compliance needs. ",
+        demo: 'https://www.figma.com/proto/9cLmfKL5LTwAWIbK9GJn1n/Privacy24?page-id=553%3A0&node-id=553%3A582&viewport=1405%2C2191%2C0.49&scaling=min-zoom',
+        github: 'https://www.figma.com/file/9cLmfKL5LTwAWIbK9GJn1n/Privacy24?node-id=553%3A582&t=JGzM7RTQ9f1keMEP-1'
     },
     {
         id: 6,
-        category: 'frontend',
+        category: 'uiux',
         image: Image6,
-        title: "Customer Journey Map (Frontend)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        title: "Customer Journey Map (UIUX)",
+        desc: "This framework is a usual go-to tactic for my team when we are trying to understand the customer journey. It is a great way to visualize the customer journey and to identify the pain points and opportunities for improvement.",
         demo: 'http://egatortutorials.com',
         github: 'https://github.com/egattor'
     },
@@ -68,27 +68,27 @@ const data = [
         category: 'frontend',
         image: Image7,
         title: "Little Lemon Restaurant (Frontend)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        desc: "This web app is made with React and was made for the capstone project during my coursera studies to become a Frontend developer. The app is a restaurant website that allows users to book a table and order food.",
+        demo: '-',
+        github: 'https://github.com/iafhurtado/little-lemon'
     },
     {
         id: 8,
-        category: 'backend',
+        category: 'frontend',
         image: Image8,
-        title: "Crown Clotthing (Backend)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Crown Clotthing (Frontend)",
+        desc: "This is part of my Frontend developer journey. This is a React app that allows users to shop for clothes. The app is connected to a backend server that allows users to sign up, sign in, add items to their cart, and checkout. It uses Redux and hooks to manage the state of the app.",
+        demo: '-',
+        github: 'https://github.com/iafhurtado/crwn-clothing-v2-lesson-10'
     },
     {
         id: 9,
-        category: 'frontend',
+        category: 'product management',
         image: Image9,
-        title: "Xocolatl Mexican Stablecoin (Frontend)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Xocolatl Mexican Stablecoin (Product Management)",
+        desc: "This project was born during ETHMexico 2021. The goal of this project was to create a mexican stablecoin that would be backed by (Wrapped) Ether. The project was built with Svelte and uses the Metamask wallet to connect to the blockchain.",
+        demo: 'https://xocolatl.finance/',
+        github: 'https://github.com/La-DAO'
     }
 ]
 
