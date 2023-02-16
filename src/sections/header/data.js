9bbf7f7e91d81e66f4b60fde12937a13d7ce24db
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-16 12:37:22 +0100

    updated socials

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,13 +1,13 @@
-import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
 import {AiOutlineTwitter} from 'react-icons/ai'
 import {AiOutlineDribbble} from 'react-icons/ai'
 import {AiFillGithub} from 'react-icons/ai'
 
 const data = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
-    {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
-    {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
+    {id: 1, link: 'https://www.linkedin.com/in/ivan-flores-hurtado-09243b44/', icon: <AiOutlineLinkedin/>},
+    {id: 2, link: 'https://twitter.com/iafhurtado', icon: <AiOutlineTwitter/>},
+    {id: 3, link: 'https://dribbble.com/iafhurtado', icon: <AiOutlineDribbble/>},
+    {id: 4, link: 'https://github.com/iafhurtado', icon: <AiFillGithub/>}
 ]
 
 export default data
