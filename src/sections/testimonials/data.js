aa93a2503e60ac69656903976e5252b860ece93f
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-16 11:14:27 +0100

    changed testimonial inputs

--- src/sections/testimonials/data.js
+++ src/sections/testimonials/data.js
@@ -10,52 +10,52 @@ import Avatar7 from '../../assets/avatar7.jpg'
 const data = [
         {
         id: 1,
-        quote: "It's truly amazing how you did exactly what I want with the little information I gave. You'll be my go-to developer from today. Highly recommend!",
+        quote: "Working with Ivan was fantastic. Due to his professional and friendly manner, he was the perfect project manager. He brought our project with international partners to a successful conclusion. I am already looking forward to the next cooperation with Ivan.",
         avatar: Avatar1,
-        name: 'Nana Ama McBrown',
-        profession: 'Actress'
+        name: 'Andrea Prinz',
+        profession: 'Business Owner'
         },
         {
         id: 2,
-        quote: "Good job, Hajia! Right on time! I really like how the project turned out, and my customers like it as well. It was fun collaborating with you.",
+        quote: "It was great working with Ivan! Very talented and hardworking.",
         avatar: Avatar2,
-        name: 'Ernest Achiever',
-        profession: 'Web Developer'
+        name: 'Laraib Lohdi',
+        profession: 'Project Manager'
         },
         {
         id: 3,
-        quote: "Impressive work, buddy. Highly recommend!",
+        quote: "Ivan did a great job with the team. He was always prepared and brought a lot of his knowledge to the table. I will hire him again.",
         avatar: Avatar3,
-        name: 'Daniel Vinyo',
-        profession: 'Graphic Designer'
+        name: 'Ammar Darkazanli',
+        profession: 'Chief Executive Officer'
         },
         {
         id: 4,
-        quote: "Wow. You finished earlier than you said. Everything's modern and beautifully done to the T. Great profession!",
+        quote: "We got to know Mr. Hurtado as a very competent and very qualified employee for our requirements. He was able to familiarize himself quickly with the tasks, enriched them with his own ideas and thus contributed significantly to the development of our projects. He was always polite and friendly to our customers. We wish Mr. Hurtado all the best for the future and we'll hire him definitely again for further projects.",
         avatar: Avatar4,
-        name: 'Diana Ayi',
-        profession: 'Singer'
+        name: 'Elke Junghans',
+        profession: 'Chief of Staff'
         },
         {
         id: 5,
-        quote: "I like how the app turned out. You are incredible, Hajia.",
+        quote: "I like how the app turned out. You are incredible, Ivan.",
         avatar: Avatar5,
         name: 'Vivian Mensah',
-        profession: 'Street Photographer'
+        profession: 'Product Marketer'
         },
         {
         id: 6,
-        quote: "Best developer in the world!",
+        quote: "Ivan provided valuable input and constructive criticism during the focus group.",
         avatar: Avatar6,
-        name: 'Hajia Bintu',
-        profession: 'Brand Influencer'
+        name: 'Adam Felouss',
+        profession: 'UX Researcher'
         },
         {
         id: 7,
-        quote: "Keep doing great stuff, dear! Nicely executed!",
+        quote: "Ivan is an experienced Product Manager. He talked about his experience with confidence and depth.",
         avatar: Avatar7,
         name: 'Nicki Nyarko',
-        profession: 'Makeup Artist'
+        profession: 'Business Analyst'
         }
     ]
 
