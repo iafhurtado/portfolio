479b60c89856d24a778fe6fc70d44692256bd29c
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-20 15:19:51 +0100

    added Upwork

--- src/sections/contact/data.js
+++ src/sections/contact/data.js
@@ -1,10 +1,10 @@
 import {HiOutlineMail} from 'react-icons/hi'
-import {RiMessengerLine} from 'react-icons/ri'
+import {SiUpwork} from 'react-icons/si'
 import {FaWhatsapp} from 'react-icons/fa'
 
 const data = [
     {id: 1, icon: <HiOutlineMail/>, link: 'mailto:iafhurtado@gmail.com'},
-    {id: 2, icon: <RiMessengerLine/>, link: 'https://www.messenger.com/t/697335550'},
+    {id: 2, icon: <SiUpwork/>, link: 'https://www.upwork.com/freelancers/~01b830b8c51f54f474'},
     {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/+4917623970774'}
 ]
 
