abc830f92a8762b82a4896d4e9bafc5f3a6c91f3
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-08-03 13:11:04 +0200

    added Calendly instead of Twitter

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,11 +1,11 @@
 import {TfiLinkedin} from 'react-icons/tfi'
-import {AiOutlineTwitter} from 'react-icons/ai'
+import {BsCalendarPlusFill} from 'react-icons/bs'
 import {GrMedium} from 'react-icons/gr'
 import {AiFillGithub} from 'react-icons/ai'
 
 const data = [
     {id: 1, link: 'https://www.linkedin.com/in/ivan-flores-hurtado-09243b44/', icon: <TfiLinkedin/>},
-    {id: 2, link: 'https://twitter.com/iafhurtado', icon: <AiOutlineTwitter/>},
+    {id: 2, link: 'https://calendly.com/ivanflores/30min', icon: <BsCalendarPlusFill/>},
     {id: 3, link: 'https://medium.com/@ivanalfredo', icon: <GrMedium/>},
     {id: 4, link: 'https://github.com/iafhurtado', icon: <AiFillGithub/>}
 ]
